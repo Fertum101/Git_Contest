6235d39589b6de24fd8373f0e68c8492955788e7
Danila Chushkin <danya_1801@mail.ru>
2023-06-18 15:18:15 +0700

    Added comments
    Added comments

--- H/H.c
+++ H/H.c
@@ -12,8 +12,10 @@ typedef struct {
 Securities database[100000];
 int dbSize = 0;
 
+// Функция для добавления новой ценной бумаги в базу данных
 void addSecurities(char* code) {
     for (int i = 0; i < dbSize; i++) {
+        // Проверяем, существует ли уже ценная бумага с данным кодом и не была ли она отозвана
         if (strcmp(database[i].code, code) == 0 && !database[i].active) {
             database[i].active = 1;
             printf("OK %s %d %d\n", code, database[i].id, database[i].reliability);
@@ -21,6 +23,7 @@ void addSecurities(char* code) {
         }
     }
 
+    // Ценная бумага с данным кодом не существует или была отозвана, добавляем новую
     strcpy(database[dbSize].code, code);
     database[dbSize].id = dbSize;
     database[dbSize].reliability = 0;
@@ -29,8 +32,10 @@ void addSecurities(char* code) {
     dbSize++;
 }
 
+// Функция для удаления ценной бумаги из базы данных
 void deleteSecurities(char* code) {
     for (int i = 0; i < dbSize; i++) {
+        // Проверяем, существует ли ценная бумага с данным кодом и активна ли она
         if (strcmp(database[i].code, code) == 0 && database[i].active) {
             database[i].active = 0;
             printf("OK %d %d\n", database[i].id, database[i].reliability);
@@ -40,9 +45,12 @@ void deleteSecurities(char* code) {
     printf("BAD REQUEST\n");
 }
 
+// Функция для обновления надежности ценной бумаги
 void updateReliability(char* code, long int reliability) {
     for (int i = 0; i < dbSize; i++) {
+        // Проверяем, существует ли ценная бумага с данным кодом и активна ли она
         if (strcmp(database[i].code, code) == 0 && database[i].active) {
+            // Обновляем надежность ценной бумаги
             database[i].reliability += reliability;
             printf("OK %d %d\n", database[i].id, database[i].reliability);
             return;
@@ -51,12 +59,14 @@ void updateReliability(char* code, long int reliability) {
     printf("BAD REQUEST\n");
 }
 
+// Функция для поиска ценной бумаги по порядковому номеру
 void findSecurities(int n) {
     if (dbSize == 0) {
         printf("EMPTY\n");
         return;
     }
 
+    // Сортируем ценные бумаги по надежности (в убывающем порядке) и идентификатору (в возрастающем порядке)
     for (int i = 0; i < dbSize - 1; i++) {
         for (int j = 0; j < dbSize - i - 1; j++) {
             if (database[j].reliability < database[j + 1].reliability || (database[j].reliability == database[j + 1].reliability && database[j].id > database[j + 1].id)) 
@@ -68,14 +78,17 @@ void findSecurities(int n) {
         }
     }
 
+    // Если бумага не активна, то ищем первую активную бумагу далее по списку
     while (database[n].active != 1) {
         n++;
         if (n >= dbSize) break;
     }
     
+    // Запрошенный порядковый номер больше, чем количество ценных бумаг в базе данных
     if (n >= dbSize) {
         while (database[n].active != 1) {
             n--;
+            // Если в базе не осталось не отозванных бумаг
             if (n < 0){
                 printf("EMPTY\n");
                 return;
@@ -83,6 +96,7 @@ void findSecurities(int n) {
         }
         printf("OK %s %d %d\n", database[n].code, database[n].id, database[n].reliability);
     } else {
+        // Находим ценную бумагу с заданным порядковым номером
         printf("OK %s %d %d\n", database[n].code, database[n].id, database[n].reliability);
     }
 }
