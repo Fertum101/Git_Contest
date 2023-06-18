8871ed3c2108bbc22cd8ec9d0cebb68029d2a622
Danila Chushkin <danya_1801@mail.ru>
2023-06-18 15:31:04 +0700

    Added comments
    Added comments

--- L/L.c
+++ L/L.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Структура узла дерева
 typedef struct Node {
     int data;            
     struct Node* left;   
     struct Node* right;   
 } Node;
 
+// Создание нового узла
 Node* createNode(int value) {
     Node* newNode = (Node*)malloc(sizeof(Node));
     newNode->data = value;
@@ -15,28 +17,27 @@ Node* createNode(int value) {
     return newNode;
 }
 
+// Вставка нового элемента в дерево
 Node* insert(Node* root, int value) {
-    if (root == NULL) {
-        return createNode(value);
-    }
+    if (root == NULL) return createNode(value);
 
     if (value < root->data) {
-        root->left = insert(root->left, value);
+        root->left = insert(root->left, value); // Если значение меньше текущего узла, вставляем в левое поддерево
     } else if (value > root->data) {
-        root->right = insert(root->right, value);
+        root->right = insert(root->right, value); // Если значение больше текущего узла, вставляем в правое поддерево
     }
 
     return root;
 }
 
+// Расчет высоты дерева
 int maxHeight(Node* root) {
-    if (root == NULL) {
-        return 0; 
-    }
+    if (root == NULL) return 0; 
 
-    int leftHeight = maxHeight(root->left);
-    int rightHeight = maxHeight(root->right);   
+    int leftHeight = maxHeight(root->left); // Рекурсивно вычисляем высоту левого поддерева
+    int rightHeight = maxHeight(root->right); // Рекурсивно вычисляем высоту правого поддерева
 
+    // Высота дерева - максимум из высот поддеревьев
     if (leftHeight > rightHeight) return(leftHeight + 1);
     else return(rightHeight + 1);  
 }
@@ -47,14 +48,15 @@ int main() {
 
     while (1) {
         scanf("%d", &num);
+        // Считывание чисел, пока не встретится 0
         if (num == 0) {
-            break;    
+            break; 
         }
         root = insert(root, num);   
     }
-
-    int height = maxHeight(root);   
-    printf("%d\n", height);         
+    
+    // Вычисление высоты дерева
+    printf("%d\n", maxHeight(root));         
 
     return 0;
 }
